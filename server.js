a04faf8a3772c278fc7696719eec0715168aba2e
Yuan <zhangyuan@users.noreply.github.com>
2017-06-09 23:11:20 +0800

    only reload pipelines for changes

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ import cors from "kcors"
 import IO from 'koa-socket'
 import watch from 'node-watch'
 const _ = require('koa-route');
+import fs from "fs";
+import jsondiffpatch from "jsondiffpatch";
+
+const store_path = path.join(__dirname, "pipelines.json");
+
+function load_pipelines() {
+  const text = fs.readFileSync(store_path, {encoding: "utf-8"});
+  return JSON.parse(text);
+}
+
+let store = load_pipelines();
 
 const app = new Koa();
 const io = new IO();
@@ -13,10 +24,13 @@ io.attach( app );
 
 app.use(cors());
 
-const store_path = path.join(__dirname, "pipelines.json");
-
 watch(store_path, () => {
-  io.broadcast("hi");
+  var pipelines = load_pipelines();
+
+  if(jsondiffpatch.diff(store, pipelines)) {
+    store = pipelines;
+    io.broadcast("hi");
+  }
 });
 
 app.use(_.get('/api/pipelines', async (ctx) => {
