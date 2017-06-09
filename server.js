de6dac36168825292a8197b1a750dba922f9d745
Yuan <zhangyuan@users.noreply.github.com>
2017-06-09 19:46:55 +0800

    use socket.io to notify client to reload

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const Koa = require('koa');
-const path = require("path")
+const path = require("path");
 import sendfile from 'koa-sendfile';
 import cors from "kcors"
+import IO from 'koa-socket'
+import watch from 'node-watch'
 const _ = require('koa-route');
 
 const app = new Koa();
+const io = new IO();
+
+io.attach( app );
+
 app.use(cors());
 
+const store_path = path.join(__dirname, "pipelines.json");
+
+watch(store_path, () => {
+  io.broadcast("hi");
+});
+
 app.use(_.get('/api/pipelines', async (ctx) => {
-  const store_path = path.join(__dirname, "pipelines.json");
   var stats = await sendfile(ctx, store_path);
 
   if(!stats) {
