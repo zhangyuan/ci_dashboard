cff704172f084934961cc3d7fa3b3a46bb8d551e
Yuan <zhangyuan@users.noreply.github.com>
2017-06-09 19:07:19 +0800

    create simple dashboard

--- server.js
+++ server.js
@@ -2,18 +2,23 @@ const Koa = require('koa');
 const path = require("path")
 import sendfile from 'koa-sendfile';
 import cors from "kcors"
+const _ = require('koa-route');
 
 const app = new Koa();
 app.use(cors());
 
-app.use(async (ctx) => {
+app.use(_.get('/api/pipelines', async (ctx) => {
   const store_path = path.join(__dirname, "pipelines.json");
   var stats = await sendfile(ctx, store_path);
 
   if(!stats) {
     ctx.body = [];
   }
-});
+}));
 
+app.use(_.get('/', async (ctx) => {
+  const index_path = path.join(__dirname, "index.html");
+  await sendfile(ctx, index_path)
+}));
 
 app.listen(3000);
